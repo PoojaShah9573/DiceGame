c192257191cc9ed3271adb4f0bdf6e3eaa53d34c
PoojaShah9573 <80915910+PoojaShah9573@users.noreply.github.com>
2021-11-17 23:11:53 +0545

    Update new.js

--- new.js
+++ new.js
@@ -1,16 +1,16 @@
 function myfunc()
 {
-var randomNumber1=Math.floor(Math.random()*6)+1; //1-6
-var randomDiceImage= "dice" + randomNumber1 +".png";  //dice1-dice-6
-var randomImagesorce= "images/" + randomDiceImage;  //images/dice1-dice6.png
-var image1=document.querySelectorAll('img')[0];
-image1.setAttribute("src", randomImagesorce);
+//var randomNumber1=Math.floor(Math.random()*6)+1; //1-6
+//var randomDiceImage= "dice" + randomNumber1 +".png";  //dice1-dice-6
+//var randomImagesorce= "images/" + randomDiceImage;  //images/dice1-dice6.png
+//var image1=document.querySelectorAll('img')[0];
+//image1.setAttribute("src", randomImagesorce);
   
   
   
-//var randomNumber1=Math.floor(Math.random()*6)+1;
-//var randomImagesorce1="images/dice" + randomNumber2 +".png";
-//document.querySelectorAll('img')[1].setAttribute("src", randomImagesorce1);
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImagesorce1="images/dice" + randomNumber2 +".png";
+document.querySelectorAll('img')[0].setAttribute("src", randomImagesorce1);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomImagesorce2="images/dice" + randomNumber2 +".png";
